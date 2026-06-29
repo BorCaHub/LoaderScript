332771852623e365109154eeff9f9d4ce2412c0e
BorcaBOYYY <xenofilsg@gmail.com>
2026-06-29 16:20:00 +0700

    Update Export.lua to load TDS_Macro

--- Loader/Script/Premium/FolderGame/TowerDefensiSimulator/Export.lua
+++ Loader/Script/Premium/FolderGame/TowerDefensiSimulator/Export.lua
@@ -1,1 +1,2 @@
-
+-- Export helper for TDS Macro
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BorCaHub/BorcaScriptHub/main/Loader/Script/Premium/FolderGame/TowerDefensiSimulator/TDS_Macro.lua"))()
