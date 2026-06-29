192b7d920c1182f0d2f58d347841bc6e1715f596
BorcaBOYYY <xenofilsg@gmail.com>
2026-06-29 16:34:31 +0700

    Add Export.lua to Free TDS folder

---
+++ Loader/Script/Free/FolderGame/TowerDefensiSimulator/Export.lua
@@ -0,0 +1,3 @@
+-- // Export.lua - TDS Macro (Free)
+-- // Redirects to main TDS_Macro script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BorCaHub/BorcaScriptHub/main/Loader/Script/Free/FolderGame/TowerDefensiSimulator/TDS_Macro.lua"))()
